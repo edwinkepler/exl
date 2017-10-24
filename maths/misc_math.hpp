736dcb34f67538d215c8933dc9f141ed8ec5d022
edwinkepler <edwinkepler@protonmail.com>
2017-10-24 13:41:26 +0200

    Add comments and license block

--- maths/misc_math.hpp
+++ maths/misc_math.hpp
@@ -1,8 +1,34 @@
+/**
+ * Misc math functions
+ * Copyright (c) Edwin Kepler, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.
+ */
+
 #ifndef MISC_MATH_HPP
 #define MISC_MATH_HPP
 
+/**
+ * @brief Namespace containing all Extended Libraries.
+ */
 namespace exl
 {
+    /**
+     * @brief Function to compute factorial.
+     * @param Integer to compute factorial from.
+     * @return Factorial.
+     */
     long long factorial(int);
 }
 
